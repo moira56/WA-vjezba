72361fe196bcd1661ee5a8281af864b4dce59697
moira56 <mcekada1@student.unipu.hr>
2024-10-26 12:40:07 +0200

    Zadatak 1
    Zadatak za vjezbu 1

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const express = require ("express");
+const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = 3001;
 
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:${PORT}');
+});
+
 app.get("/", (req, res) => {
-    res.send("Hello, world!");
-    });
-    app.listen(PORT, (error) => {
-    if (error) {
-    console.error('Greška prilikom pokretanja poslužitelja: ${error.message}');
-    } else {
-    console.log('Server je pokrenut na http://localhost:${PORT}');
-    }
-    });
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+app.get("/users", (req, res) => {
+    const users = [
+        { id: 1, ime: "Moira", prezime: "Čekada" },
+        { id: 2, ime: "Monika", prezime: "Čekada" },
+        { id: 3, ime: "Moira", prezime: "Monika" }
+    ];
+    res.json(users);
+});
 
